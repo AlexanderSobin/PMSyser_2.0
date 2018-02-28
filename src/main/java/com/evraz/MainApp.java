078d7bf101033668b389851fd1ed3d72e0bef3b5
Alexander <Aleksander.Sobin@evraz.com>
2018-02-28 13:06:11 +0300

    edited title

--- src/main/java/com/evraz/MainApp.java
+++ src/main/java/com/evraz/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
         String fxmlFile = "/fxml/gui.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("PMSyser 2.0");
         stage.setScene(new Scene(root));
         stage.show();
     }
